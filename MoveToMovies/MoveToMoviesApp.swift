7eadc9e8a2db9cb30adbd042dedd7f4fd652e5f9
vegiwoo <vegiwoo@protonmail.com>
2021-03-04 20:11:09 +0300

    Initial commit

---
+++ MoveToMovies/MoveToMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveToMoviesApp.swift
+//  MoveToMovies
+//
+//  Created by Dmitry Samartcev on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoveToMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
